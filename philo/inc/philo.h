6672751b06917b29b2bb5a47cb47833e4839c9b3
yanel <yanelperaltarodriguez77@gmail.com>
2025-05-27 12:52:44 +0200

    first commit

---
+++ philo/inc/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaperalt <yaperalt@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 18:28:11 by yaperalt          #+#    #+#             */
+/*   Updated: 2025/05/27 12:52:02 by yaperalt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>     // memset
+# include <stdio.h>      // printf
+# include <stdlib.h>     // malloc, free
+# include <unistd.h>     // write, usleep
+# include <sys/time.h>   // gettimeofday
+# include <pthread.h>    // pthread_* functions
+
+
+
+#endif
